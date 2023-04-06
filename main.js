f9b7f9d74755216cd5d1c3b8a714165c02bd61b9
Lena Younger <lenayounger667@gmail.com>
2023-04-06 15:22:47 +0100

    Add existing project files to Git

---
+++ main.js
@@ -0,0 +1,20 @@
+//decaration of variable
+let navBar = document.querySelector('.navbar');
+
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navBar.classList.toggle('active')
+}
+
+
+
+
+
+//-- -------js for product gallery----- --
+
+
+
+
+
+//js for toggle form
+
